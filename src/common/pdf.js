4d3face733aca12c333281c3549b34a37dcb1ed7
David <herr.talbot@gmail.com>
2018-02-18 00:52:21 -0500

    Generating new page for pdf as needed. Now passing filters as paramz to calendar deeplink

--- src/common/pdf.js
+++ src/common/pdf.js
@@ -2,16 +2,17 @@ import jsPDF from 'jspdf';
 import I18N from './i18n';
 import datePack from './date.i18n.json';
 
-class PDFGenerator {  
+class PDFGenerator {
   constructor() {
     this.pdf = new jsPDF({unit: 'in'});
+    this.y = 0;
   }
 
   saveRecord(record, pack, name) {
     this.pdf.text(I18N.format('record-title', pack, record.entry), 0.3, 0.42);
     this.pdf.text(`${record.number} / ${record.total}`, 6.8, 0.42);
     this.addLine(0, 8.5, 0.75, 0.05);
-    this.addTextLine(I18N.get('record-date', pack), I18N.format('record-date-format', pack, 
+    this.addTextLine(I18N.get('record-date', pack), I18N.format('record-date-format', pack,
       I18N.get('day-' + record.date.dayName.toLowerCase(), datePack),
       parseInt(record.date.day, 10),
       I18N.get('month-' + record.date.month, datePack),
@@ -19,28 +20,31 @@ class PDFGenerator {
 
     this.addTextLine(I18N.get('record-theater', pack), record.theater ? record.theater : I18N.get('record-not-available', pack), 1.7);
     this.addLine(0.3, 8, 1.9, 0.01);
-    let y = 2.2;        
+    this.y = 2.2;
     record.pieces.forEach(piece => {
       Object.keys(piece).forEach(key => {
-        this.addTextLine(I18N.get('record-' + key, pack), piece[key], y);
-        y += 0.5;
-      });  
-      y -= 0.3;
-      this.addLine(0.3, 8, y, 0.01);  
-      y += 0.4;
+        this.addTextLine(I18N.get('record-' + key, pack), piece[key], this.y);
+        this.y += 0.5;
+      });
+      this.y -= 0.3;
+      this.addLine(0.3, 8, this.y, 0.01);
+      this.y += 0.4;
+      this.checkPage();
     });
-    this.addTextLine(I18N.get('record-receipts', pack), I18N.format('record-pounds', pack, record.receipts), y);
-    y += 0.3;
+    this.addTextLine(I18N.get('record-receipts', pack), I18N.format('record-pounds', pack, record.receipts), this.y);
+    this.y += 0.3;
+    this.checkPage();
     this.pdf.setFontSize(12);
     Object.keys(record.entries).forEach(entry => {
-      this.addSmallTextLine(pack['record-' + entry] ? I18N.get('record-' + entry, pack) : entry, 
-        I18N.format('record-audiences', pack, record.entries[entry].quantity), 
+      this.addSmallTextLine(pack['record-' + entry] ? I18N.get('record-' + entry, pack) : entry,
+        I18N.format('record-audiences', pack, record.entries[entry].quantity),
         record.entries[entry].price,
         record.entries[entry].total,
-        y);
-      y += 0.3;
+        this.y);
+        this.y += 0.3;
+        this.checkPage();
     });
-    this.pdf.save(name + '.pdf');    
+    this.pdf.save(name + '.pdf');
   }
 
   addLine(x1, x2, y, width) {
@@ -71,6 +75,13 @@ class PDFGenerator {
 
   addEllipsis(text, count) {
     return text.length > count ? `${text.substr(0, count - 1)} ...` : text;
+  }
+
+  checkPage() {
+    if (this.y >= 11.0) {
+      this.y = 0.42;
+      this.pdf.addPage();
+    }
   }
 }
 
--- src/results/results.record.details.js
+++ src/results/results.record.details.js
@@ -15,17 +15,17 @@ const mapStateToProps = (state) => {
 const DISPLAY_CALENDAR = 'display=2';
 const SEARCH_PARAMETER_SEPARATOR = '&';
 
-class ResultsRecordDetails extends Component {  
+class ResultsRecordDetails extends Component {
   render() {
     const calendarLinkSuffix = this.createCalendarDeeplink(this.props.filterOn);
-    
+
     return (
       <div className='results-record__body'>
         <div>
           <span className='results-record__label'>{I18N.get('record-date', pack)}</span>
           <span className='results-record__symbol'> : </span>
           <span className='results-record__value results-record__value--actionable'>
-            {I18N.format('record-date-format', pack, 
+            {I18N.format('record-date-format', pack,
               I18N.get('day-' + this.props.record.date.dayName.toLowerCase(), datePack),
               parseInt(this.props.record.date.day, 10),
               I18N.get('month-' + this.props.record.date.month, datePack),
@@ -33,7 +33,7 @@ class ResultsRecordDetails extends Component {
           </span>
           {this.props.record.entryLink ? <a href={this.props.record.entryLink} target='_blank'>
             <Icon iconClass={'fa-book results-record__show'} alternate={true}/>
-          </a>: null}          
+          </a>: null}
           <a href={I18N.get('record-analytics', pack) + calendarLinkSuffix} target='_blank'>
             <Icon iconClass={'fa-calendar results-record__calendar'} alternate={true}/>
           </a>
@@ -46,7 +46,7 @@ class ResultsRecordDetails extends Component {
         {this.props.record.pieces.map((piece, index) => (
           <div key={index}>
             <div className='results-record__separator'/>
-            {Object.keys(piece).map(key => 
+            {Object.keys(piece).map(key =>
               <div key={key}>
                 <span className='results-record__label'>{I18N.get('record-' + key, pack)}</span>
                 <span className='results-record__symbol'> : </span>
@@ -61,14 +61,14 @@ class ResultsRecordDetails extends Component {
           <span className='results-record__symbol'> : </span>
           <span className='results-record__value'>{I18N.format('record-pounds', pack, this.props.record.receipts)}</span>
         </div>
-        {Object.keys(this.props.record.entries).map(entry => 
+        {Object.keys(this.props.record.entries).map(entry =>
           <div key={entry} className='results-record__receipts'>
             <span title={pack['record-' + entry] ? I18N.get('record-' + entry, pack) : entry} className='results-record__label'>{pack['record-' + entry] ? I18N.get('record-' + entry, pack) : entry}</span>
             <span className='results-record__symbol'> : </span>
             <span className='results-record__number'>{I18N.format('record-audiences', pack, this.props.record.entries[entry].quantity)}</span>
             <span className='results-record__symbol'> X </span>
             <span className='results-record__number'>{this.props.record.entries[entry].price}</span>
-            <span className='results-record__symbol'> = </span>              
+            <span className='results-record__symbol'> = </span>
             <span className='results-record__number'>{this.props.record.entries[entry].total}</span>
           </div>
         )}
@@ -77,9 +77,7 @@ class ResultsRecordDetails extends Component {
   }
 
   createCalendarDeeplink(filterOn) {
-    return  '?' + DISPLAY_CALENDAR;
-    // TODO - enable once analytics tool support search parameters filtering
-    // return  '?' + Object.keys(filterOn).map(filter => `${filter}=${filterOn[filter]}`).concat(DISPLAY_CALENDAR).join(SEARCH_PARAMETER_SEPARATOR);
+    return  '?' + Object.keys(filterOn).map(filter => `${filter}=${filterOn[filter]}`).concat(DISPLAY_CALENDAR).join(SEARCH_PARAMETER_SEPARATOR);
   }
 }
 
