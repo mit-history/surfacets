6cbbfd9233ecc298536cdfb3b947bc612071a0dc
David <herr.talbot@gmail.com>
2018-04-16 20:46:43 -0400

    Fix PDF rendering for values that must spawn multiple lines.

--- src/common/pdf.js
+++ src/common/pdf.js
@@ -12,18 +12,18 @@ class PDFGenerator {
     this.pdf.text(I18N.format('record-title', pack, record.entry), 0.3, 0.42);
     this.pdf.text(`${record.number} / ${record.total}`, 6.8, 0.42);
     this.addLine(0, 8.5, 0.75, 0.05);
-    this.addTextLine(I18N.get('record-date', pack), I18N.format('record-date-format', pack,
+    this.y = this.addTextLine(I18N.get('record-date', pack), I18N.format('record-date-format', pack,
       I18N.get('day-' + record.date.dayName.toLowerCase(), datePack),
       parseInt(record.date.day, 10),
       I18N.get('month-' + record.date.month, datePack),
       record.date.year), 1.2);
 
-    this.addTextLine(I18N.get('record-theater', pack), record.theater ? record.theater : I18N.get('record-not-available', pack), 1.7);
-    this.addLine(0.3, 8, 1.9, 0.01);
-    this.y = 2.2;
+    this.y = this.addTextLine(I18N.get('record-theater', pack), record.theater ? record.theater : I18N.get('record-not-available', pack), this.y + 0.5);
+    this.addLine(0.3, 8, this.y + 0.2, 0.01);
+    this.y +=  0.6;
     record.pieces.forEach(piece => {
       Object.keys(piece).forEach(key => {
-        this.addTextLine(I18N.get('record-' + key, pack), piece[key], this.y);
+        this.y = this.addTextLine(I18N.get('record-' + key, pack), piece[key], this.y);
         this.y += 0.5;
       });
       this.y -= 0.3;
@@ -31,18 +31,19 @@ class PDFGenerator {
       this.y += 0.4;
       this.checkPage();
     });
-    this.addTextLine(I18N.get('record-receipts', pack), I18N.format('record-pounds', pack, record.receipts), this.y);
+    this.y = this.addTextLine(I18N.get('record-receipts', pack), I18N.format('record-pounds', pack, record.receipts), this.y);
     this.y += 0.3;
     this.checkPage();
     this.pdf.setFontSize(12);
     Object.keys(record.entries).forEach(entry => {
-      this.addSmallTextLine(pack['record-' + entry] ? I18N.get('record-' + entry, pack) : entry,
-        I18N.format('record-audiences', pack, record.entries[entry].quantity),
+      var qty = entry.includes('lodge') ? 'x' : record.entries[entry].quantity;
+      this.y = this.addSmallTextLine(pack['record-' + entry] ? I18N.get('record-' + entry, pack) : entry,
+        I18N.format('record-audiences', pack, qty),
         record.entries[entry].price,
         record.entries[entry].total,
         this.y);
-        this.y += 0.3;
-        this.checkPage();
+      this.y += 0.3;
+      this.checkPage();
     });
     this.pdf.save(name + '.pdf');
   }
@@ -58,12 +59,26 @@ class PDFGenerator {
     this.pdf.text(label, 0.3, y);
     this.pdf.setFontStyle('normal');
     this.pdf.text(' : ', 2, y);
-    this.pdf.text(value, 2.5, y);
+    // Iterate lines to show whole value
+    var nextLine = true;
+    for (var i = 0; nextLine; i = i + 50) {
+      var endOfLine = i + 50
+      var subValue = value.substring(i, endOfLine).trim();
+      this.pdf.text(subValue + (value.length > endOfLine ? '-' : ''), 2.5, y);
+      if (value.length > endOfLine) {
+        y = y + 0.25;
+      } else {
+        nextLine = false;
+      }
+    }
+    return y;
   }
 
   addSmallTextLine(label, count, price, total, y) {
     this.pdf.setFontStyle('bold');
-    this.pdf.text(this.addEllipsis(label, 18), 0.3, y);
+    const MAX_LENGTH = 18;
+    var subLabel = label.substring(0, MAX_LENGTH).trim();
+    this.pdf.text(subLabel + (label.length > MAX_LENGTH ? '-' : ''), 0.3, y);
     this.pdf.setFontStyle('normal');
     this.pdf.text(' : ', 2, y);
     this.pdf.text(count, 2.5, y);
@@ -71,6 +86,24 @@ class PDFGenerator {
     this.pdf.text(price, 4.5, y);
     this.pdf.text(' = ', 5.5, y);
     this.pdf.text(total, 6, y);
+    if (label.length > 20) {
+      this.pdf.setFontStyle('bold');
+      var nextLine = true;
+      y = y + 0.25;
+      for (var i = MAX_LENGTH; nextLine; i = i + MAX_LENGTH) {
+        var endOfLine = i + MAX_LENGTH;
+        var subLabel = label.substring(i, endOfLine).trim();
+        console.log('subLabel', subLabel);
+        this.pdf.text(subLabel + (label.length > endOfLine ? '-' : ''), 0.3, y);
+        if (label.length > endOfLine) {
+          y = y + 0.25;
+        } else {
+          nextLine = false;
+        }
+      }
+      this.pdf.setFontStyle('normal');
+    }
+    return y;
   }
 
   addEllipsis(text, count) {
